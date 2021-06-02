fb65fd078a191456477ce4651ca2cff702d55ecf
Aniket-123-bit <anisunny782@gmail.com>
2021-06-02 15:02:45 +0530

    Commit emailvalid or not valid push

---
+++ src/UserRegistartion.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@FunctionalInterface
+interface User {
+    void emailValidate();
+
+public class UserRegistartion {
+    public static void main(String[] args) {
+        //Lamda expression
+        User user = () -> { Scanner sc = new Scanner(System.in);
+            //Regex expression
+            String regex = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
+
+            System.out.println("Enter your email");
+            String email = sc.next();
+
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(email);
+            if(matcher.matches()) {
+                System.out.println("Given email id is valid");
+            } else {
+                System.out.println("Given email id is not valid");
+            }
+        };
+        user.emailValidate();
+    }
+
+
+}
+
+}
